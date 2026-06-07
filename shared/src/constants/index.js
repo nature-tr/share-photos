99a39e8fa70325a0216e2c59b05d6bc48bdc2c4c
dolmocheng <dolmocheng@tencent.com>
2026-06-07 23:55:54 +0800

    chore: 清理 shared/src 下误编译的 .js 文件 + 加入 gitignore

--- shared/src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export * from './share-code.js';
-export * from './limits.js';
-export * from './error-codes.js';
--- shared/src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './constants/index.js';
-export * from './dto/index.js';
-export * from './schema/index.js';
-export * from './utils/index.js';
